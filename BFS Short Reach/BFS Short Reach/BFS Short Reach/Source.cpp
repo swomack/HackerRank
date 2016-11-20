b0a222383c4cdd9f99f4ededcff89a8e1f79c6f8
swomack <sourov0805045@gmail.com>
2016-11-20 23:24:40 +0600

    **BFS Short Reach - Completed **

--- BFS Short Reach/BFS Short Reach/BFS Short Reach/Source.cpp
+++ BFS Short Reach/BFS Short Reach/BFS Short Reach/Source.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
-#include <limits>
+#include <climits>
 #include <queue>
 
 using namespace std;
@@ -12,9 +12,19 @@ using namespace std;
 
 vector<int> adj_list[MAX_NODES];
 
+bool adj_matrix[MAX_NODES][MAX_NODES];
+
 vector<int> shortest_paths(MAX_NODES);
 
 int nodes, edges;
+
+class CompareDist
+{
+public:
+	bool operator()(pair<int, int> n1, pair<int, int> n2) {
+		return n1.second>n2.second;
+	}
+};
 
 
 int main() 
@@ -31,29 +41,66 @@ int main()
 		{
 			adj_list[i].clear();
 			shortest_paths[i] = INT_MAX;
+
+			for (int j = 0; j <= nodes; j++)
+			{
+				adj_matrix[i][j] = false;
+			}
 		}
+
+		
+
+
 
 		while (edges--)
 		{
 			int a, b;
 			cin >> a >> b;
 
-			adj_list[a].push_back(b);
+			if (adj_matrix[a][b] == false) {
+				adj_list[a].push_back(b);
+				adj_list[b].push_back(a);
+
+				adj_matrix[a][b] = true;
+				adj_matrix[b][a] = true;
+			}
+			
 		}
 
 		int starting_node;
 		cin >> starting_node;
 
-		priority_queue<pair<int, int>> shortest_path_queue;
+		priority_queue < pair<int, int>, vector<pair<int, int>>, CompareDist> shortest_path_queue;
 		shortest_paths[starting_node] = 0;
 
 		shortest_path_queue.push(make_pair(starting_node, 0));
 
 		while (!shortest_path_queue.empty())
 		{
-			
+			pair<int, int> node = shortest_path_queue.top();
+			shortest_path_queue.pop();
+
+			if (shortest_paths[node.first] < node.second)
+				continue;
+
+			shortest_paths[node.first] = node.second;
+
+			for (int i = 0; i < adj_list[node.first].size(); i++)
+			{
+				shortest_path_queue.push(make_pair(adj_list[node.first][i], shortest_paths[node.first] + 6));
+			}
 		}
 
+		for (int i = 1; i <= nodes; i++)
+		{
+			if (i != starting_node) 
+			{
+				fprintf(stdout, "%d", (shortest_paths[i] != INT_MAX ? shortest_paths[i] : -1));
+				fprintf(stdout, " ");
+			}
+				
+		}
+		fprintf(stdout, "\n");
 
 	}
 
