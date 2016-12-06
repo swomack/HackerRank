9d1859a607c8c55c03d8ca95eaa5d9e8cbc88250
Rasheduzzaman Sourov <rasheduzzaman.sourov@enosisbd.com>
2016-12-06 13:26:12 +0600

    Hackerrank: Is this a valid binary search tree. * Min max

--- Is This a Binary Search Tree/Is This a Binary Search Tree/Is This a Binary Search Tree/Source.cpp
+++ Is This a Binary Search Tree/Is This a Binary Search Tree/Is This a Binary Search Tree/Source.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <climits>
 
 using namespace std;
 
@@ -11,14 +12,26 @@ struct Node
 };
 
 
-void inOrderTraverse(Node* node, vector<int>& result)
+bool isValidBSTHelper(Node* node, int min_limit, int max_limit, bool left_inclusive, bool right_inclusive)
 {
 	if (node == NULL)
-		return;
+		return true;
 
-	inOrderTraverse(node->left, result);
-	result.push_back(node->data);
-	inOrderTraverse(node->right, result);
+	bool bst;
+
+	if (left_inclusive && right_inclusive)
+		bst = node->data >= min_limit && node->data <= max_limit ? true : false;
+	else if (left_inclusive)
+		bst = node->data >= min_limit && node->data < max_limit ? true : false;
+	else if (right_inclusive)
+		bst = node->data > min_limit && node->data <= max_limit ? true : false;
+	else
+		bst = node->data > min_limit && node->data < max_limit ? true : false;
+
+	if (!bst)
+		return false;
+
+	return isValidBSTHelper(node->left, min_limit, node->data, left_inclusive, false) && isValidBSTHelper(node->right, node->data, max_limit, false, right_inclusive);
 }
 
 
@@ -27,16 +40,7 @@ bool checkBST(Node* root)
 	if (root == NULL)
 		return true;
 
-	vector<int> in_order;
-	inOrderTraverse(root, in_order);
-
-	for (int i = 0; i < in_order.size() - 1; i++)
-	{
-		if (in_order[i + 1] <= in_order[i])
-			return false;
-	}
-
-	return true;
+	return isValidBSTHelper(root->left, INT_MIN, root->data, true, false) && isValidBSTHelper(root->right, root->data, INT_MAX, false, true);
 }
 
 int main()
